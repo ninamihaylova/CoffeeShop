89b6f156fe7baa4cfa65f4e3c0ac1eab2f501be7
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 11:58:14 +0300

    Initial commit

---
+++ CoffeeShop/Program.cs
@@ -0,0 +1,26 @@
+﻿class CoffeeShop
+{
+    public static void Main(String[] args)
+    {
+        string drinkName = Console.ReadLine();
+        string extra = Console.ReadLine();
+        decimal price = 0m;
+
+
+        if (drinkName == "coffee")
+        {
+            price = 1.00m;
+        }
+        else
+        {
+            price = 0.60m;
+        }
+
+        if (extra == "sugar")
+        {
+            price += 0.40m;
+        }
+
+        Console.WriteLine($"Final price: ${price:F2}");
+    }
+}
